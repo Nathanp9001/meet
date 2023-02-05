402124eeddaf196c4568666ce07b68261dcf0e41
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2023-02-05 10:49:32 -0600

    add "details" button for events

--- src/Event.js
+++ src/Event.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = { collapsed: true };
+
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      collapsed: !prevState.collapsed,
+    }));
+  };
   render() {
     const { event } = this.props;
+    const { collapsed } = this.state;
 
     return(
     <div className="event">
       <p className="summary">{ event.summary }</p>
       <p className="start">{ event.start.dateTime }</p>
       <p className="location">{ event.location }</p>
+
+    <button className="details-button" onCLick={this.toggleDetails}>
+      {collapsed ? "show" : "hide"} details
+    </button>
+
+    {!collapsed && (
+      <div className="details">
+        <h3 className="about">About this event:</h3>
+        <p className="description">{event.description}</p>
+      </div>
+    )}
     </div>
     );
   }
