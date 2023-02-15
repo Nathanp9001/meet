9dc460d4f298cdabbd67e5714cb03d3ebb01613d
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2023-02-15 15:05:16 -0600

    add ability to change number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
+  state = { num: 10 };
+
+  changeNum = (value) => {
+    this.setState({ num: value });
+    this.props.updateNumberOfEvents(value);
+    if (value < 1 || value > 20) {
+      this.setState({ errorText: 'Please select a value between 1 and 20' });
+    } else this.setState({ errorText: '' });
+  };
+
+  componentDidMount () {
+    this.setState({ num: this.props.num || 10 });
+  };
+
   render () {
+    const { num } = this.state;
+
     return (
-      <div></div>
+      <div>
+        <label>
+          Number of events
+          <input
+            classNamej='num'
+            type='number'
+            value={num}
+            onChange={(event) => {
+              this.changeNum(event.target.value);
+            }}
+          />
+        </label>
+      </div>
     );
   }
 }
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -6,10 +6,32 @@ describe('<NumberOfEvents /> component', () => {
 
   let NumberOfEventsWrapper;
   beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateNumberOfEvents={() => { }} />);
   });
 
   test('render correct number of events', () => {
     expect(NumberOfEventsWrapper).toBeDefined();
   });
-});
+
+  test('default number of events value is 10', () => {
+    expect(NumberOfEventsWrapper.find('input.num').prop('type')).toBe('number');
+    expect(NumberOfEventsWrapper.state('num')).toBe(10);
+  });
+
+  test('user can change value for number of events', () => {
+    expect(numberOfEventsWrapper.state('num')).toBe(10);
+
+    NumberOfEventsWrapper.find('input.num').simulate('change', {
+      target: { value: 20 }
+    });
+    expect(NumberOfEventsWrapper.state('num')).toBe(20);
+  });
+
+  test('runder number of events equal to the value the user has chosen', () => {
+    const NumberOfEventsWrapperWithProp = shallow(
+      <NumberOfEvents num={8} updateNumberOfEvents={() => { }} />
+    );
+
+    expect(NumberOfEventsWrapperWithProps.state('num')).toBe(8);
+  });
+})
