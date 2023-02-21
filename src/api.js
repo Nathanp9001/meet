162eae1906076fe07765e5c836246a3d61d983db
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2023-02-21 14:18:22 -0600

    move "number of events" selector to top of page

--- src/api.js
+++ src/api.js
@@ -66,7 +66,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'YOUR_GET_EVENTS_API_ENDPOINT' + '/' + token;
+    const url = 'https://1vkz9nrqdh.execute-api.us-east-2.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
