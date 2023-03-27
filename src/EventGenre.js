48cccdeac11d3ee859b0fa212f49e89be55f3d44
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2023-03-27 17:07:38 -0500

    add colors for pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,6 +3,7 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
+  const colors = ['#ffa500', '#ffc0cb', '#800080', '#800000', '#dc143c']
 
   useEffect(() => {
     setData(() => {
@@ -30,6 +31,11 @@ const EventGenre = ({ events }) => {
           dataKey="value"
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
         >
+          {
+            data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index]}/>
+            ))
+          }
         </Pie>
       </PieChart>
     </ResponsiveContainer>
